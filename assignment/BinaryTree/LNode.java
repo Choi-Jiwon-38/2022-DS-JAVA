44c92eed0505d5c7d4bfd972f205a80184d88103
죄죄원 <81795729+Choi-Jiwon-38@users.noreply.github.com>
2022-04-14 09:59:18 +0900

    change INode -> LNode

---
+++ assignment/BinaryTree/LNode.java
@@ -0,0 +1,36 @@
+package BinaryTree;
+
+public class LNode<E> implements BinNode<E> {
+    private E data;
+
+    public LNode(E data) {
+        this.data = data;
+    }
+
+    @Override
+    public E element() {
+        return data;
+    }
+
+    @Override
+    public E setElement(E item) {
+        return this.data = item;
+    }
+
+    @Override
+    public BinNode<E> left() {
+        return null;
+    }
+
+    @Override
+    public BinNode<E> right() {
+        return null;
+    }
+
+    @Override
+    public boolean isLeaf() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+    
+}
--- assignment/BinaryTree/TreeTest.java
+++ assignment/BinaryTree/TreeTest.java
@@ -2,10 +2,10 @@ package BinaryTree;
 
 public class TreeTest {
     public static void main(String[] args) {
-        BinNode<String> D = new INode<String>("D", null, null);
-        BinNode<String> G = new INode<String>("G", null, null);
-        BinNode<String> H = new INode<String>("H", null, null);
-        BinNode<String> I = new INode<String>("I", null, null);
+        BinNode<String> D = new LNode<String>("D");
+        BinNode<String> G = new LNode<String>("G");
+        BinNode<String> H = new LNode<String>("H");
+        BinNode<String> I = new LNode<String>("I");
         BinNode<String> B = new INode<String>("B", null, D);
         BinNode<String> E = new INode<String>("E", G, null);
         BinNode<String> F = new INode<String>("F", H, I);
