d6b625baac95c97cf4ba444403b777d542e7140d
죄죄원 <81795729+Choi-Jiwon-38@users.noreply.github.com>
2022-04-14 09:54:01 +0900

    add preorder, inorder, postorder

--- assignment/BinaryTree/TreeTest.java
+++ assignment/BinaryTree/TreeTest.java
@@ -14,5 +14,36 @@ public class TreeTest {
 
         System.out.println(A.right().left().left().element()); // A -> C -> E -> G
         System.out.println(A.right().right().left().element()); // A -> C -> F -> H
+    
+        preorder(A); // A B D C E G F H I
+        System.out.println();
+        inorder(A); // B D A G E C H F I
+        System.out.println();
+        postorder(A); // D B G E H I F C A
+        System.out.println();
+    }
+
+    public static <E> void preorder(BinNode<E> rt) {
+        if (rt == null) return;
+        System.out.print(rt.element()); // visit
+        System.out.print(" ");
+        preorder(rt.left());
+        preorder(rt.right());
+    }
+
+    public static <E> void inorder(BinNode<E> rt) {
+        if (rt == null) return;        
+        inorder(rt.left());
+        System.out.print(rt.element()); // visit
+        System.out.print(" ");
+        inorder(rt.right());
+    }
+
+    public static <E> void postorder(BinNode<E> rt) {
+        if (rt == null) return;        
+        postorder(rt.left());
+        postorder(rt.right());
+        System.out.print(rt.element()); // visit
+        System.out.print(" ");
     }
 }
